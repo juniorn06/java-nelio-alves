fe82c003ea845e03dfc849972b096e769d4c0032
Junior <juniorn06@gmail.com>
2021-08-10 10:46:57 -0300

    Informações do caminho do arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+
 import java.io.File;
 import java.util.Scanner;
 
@@ -8,26 +9,11 @@ public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a folder path: ");
         String strPath = sc.nextLine();
 
         File path = new File(strPath);
-
-        File[] folders = path.listFiles(File::isDirectory); //isDirectory: Função que lista apenas o que é diretório ou pasta.
-        System.out.println("FOLDERS: ");
-        for (File folder: folders){
-            System.out.println(folder);
-        }
-
-        File[] files = path.listFiles(File::isFile); //isFile: Função que lista apenas os arquivos contidos nas pastas.
-        System.out.println();
-        System.out.println("FILES: ");
-        for (File file: files){
-            System.out.println(file);
-        }
-
-        boolean success = new File(strPath + "\\subdir").mkdir();
-        System.out.println();
-        System.out.println("Directory created successfully: " + success);
+        System.out.println("getName: " + path.getName()); // getName: exibe apenas o nome do arquivo.
+        System.out.println("getParent: " + path.getParent()); // getParent: exibe apenas o caminho do arquivo.
+        System.out.println("getPath: " + path.getPath()); //getPath: exibe o caminho completo.
     }
 }
